c7057b41a5080e70135df750c7ab754b1160c5eb
Rahul <rahulhari1252@gmail.com>
2024-07-12 12:07:28 +0530

    commit

--- frontend/src/ui/DescriptionCard.jsx
+++ frontend/src/ui/DescriptionCard.jsx
@@ -1,16 +1,17 @@
-import React from 'react'
-import { Stack } from '@mui/material'
-import Box from '@mui/material'
+import React from 'react';
+import { Stack, Box, Typography } from '@mui/material';
+
 export default function DescriptionCard() {
   return (
-    <>
-    <Box sx={{ width: '100%' }}>
+    <Box>
       <Stack spacing={2}>
-        <Item>Item 1</Item>
-        <Item>Item 2</Item>
-        <Item>Item 3</Item>
+        <Typography variant="h5" component="h2">
+          Description
+        </Typography>
+        <Typography variant="body1">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </Typography>
       </Stack>
     </Box>
-    </>
-  )
+  );
 }
