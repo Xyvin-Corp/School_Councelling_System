63361a09a85bfeb7c23a521fcba00be479931aa3
anjanavu <anjanavu2000@gmail.com>
2024-07-13 12:19:04 +0530

    add theme

--- frontend/src/theme/Theme.jsx
+++ frontend/src/theme/Theme.jsx
@@ -4,7 +4,6 @@ import { CssBaseline } from "@mui/material";
 const globalStyles = `
   @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap');
   @import url('https://fonts.googleapis.com/css2?family=STIX+Two+Text&display=swap');
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100..900&display=swap');
 `;
 
 const theme = createTheme({
@@ -17,12 +16,10 @@ const theme = createTheme({
       fontWeight: 600,
     },
     h2: {
-      fontFamily: "'Roboto'",
       fontSize: "40px",
       fontWeight: 600,
     },
     h3: {
-      fontFamily: "Roboto",
       fontSize: "20px",
       fontWeight: 600,
     },
@@ -32,10 +29,9 @@ const theme = createTheme({
     },
     h5: {
       fontSize: "16px",
-      fontWeight: 400,
+      fontWeight: 600,
     },
     h6: {
-      fontFamily: "Roboto",
       fontSize: "14px",
       fontWeight: 400,
     },
@@ -44,11 +40,7 @@ const theme = createTheme({
       fontWeight: 400,
     },
     h8: {
-      fontSize: "14px",
-      fontWeight: 400,
-    },
-    h9: {
-      fontSize: "9px",
+      fontSize: "10px",
       fontWeight: 600,
     },
   },
